bc9d9f50c00a186489d7ba9ae0ca50933acf0652
Tom Hartnett <tomhartnett@gmail.com>
2020-08-08 17:14:52 -0500

    Initial Commit

---
+++ WatchList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchList
+//
+//  Created by Tom Hartnett on 8/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
