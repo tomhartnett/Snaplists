ca3487bae60eaf1782c2954e2bae4cc97e0b92b8
Tom Hartnett <tomhartnett@gmail.com>
2020-08-16 08:33:02 -0500

    Implemented watch complications.

--- WatchApp Extension/Complication/ComplicationController.swift
+++ WatchApp Extension/Complication/ComplicationController.swift
@@ -25,19 +25,88 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
-    }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
-        // Call the handler with the timeline entries after the given date
-        handler(nil)
+
+        var entry: CLKComplicationTimelineEntry?
+
+        if let template = getTemplate(for: complication) {
+            entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+        }
+
+        handler(entry)
     }
 
     // MARK: - Sample Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+
+        let template = getTemplate(for: complication)
+
+        handler(template)
+    }
+}
+
+private extension ComplicationController {
+    func getTemplate(for complication: CLKComplication) -> CLKComplicationTemplate? {
+
+        var template: CLKComplicationTemplate?
+
+        switch complication.family {
+        case .circularSmall:
+            let t = CLKComplicationTemplateCircularSmallSimpleImage()
+            let image = UIImage(named: "Complication/Circular") ?? UIImage()
+            t.imageProvider = CLKImageProvider(onePieceImage: image)
+            template = t
+        case .modularSmall:
+            let t = CLKComplicationTemplateModularSmallSimpleImage()
+            let image = UIImage(named: "Complication/Modular") ?? UIImage()
+            t.imageProvider = CLKImageProvider(onePieceImage: image)
+            template = t
+        case .modularLarge:
+            break
+        case .utilitarianSmall:
+            let t = CLKComplicationTemplateUtilitarianSmallSquare()
+            let image = UIImage(named: "Complication/Utilitarian") ?? UIImage()
+            t.imageProvider = CLKImageProvider(onePieceImage: image)
+            template = t
+        case .utilitarianSmallFlat:
+            break
+        case .utilitarianLarge:
+            let t = CLKComplicationTemplateUtilitarianLargeFlat()
+            t.textProvider = CLKSimpleTextProvider(text: "• Simplists")
+            template = t
+        case .extraLarge:
+            let t = CLKComplicationTemplateExtraLargeSimpleImage()
+            let image = UIImage(named: "Complication/Extra Large") ?? UIImage()
+            t.imageProvider = CLKImageProvider(onePieceImage: image)
+            t.imageProvider.tintColor = .white
+            template = t
+        case .graphicCorner:
+            let t = CLKComplicationTemplateGraphicCornerTextImage()
+            let image = UIImage(named: "Complication/Graphic Corner") ?? UIImage()
+            t.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+            t.textProvider = CLKSimpleTextProvider(text: "Simplists")
+            template = t
+        case .graphicBezel:
+            let t = CLKComplicationTemplateGraphicBezelCircularText()
+            let c = CLKComplicationTemplateGraphicCircularImage()
+            let image = UIImage(named: "Complication/Graphic Circular") ?? UIImage()
+            c.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+            t.circularTemplate = c
+            t.textProvider = CLKSimpleTextProvider(text: "Simplists")
+            template = t
+        case .graphicCircular:
+            let t = CLKComplicationTemplateGraphicCircularImage()
+            let image = UIImage(named: "Complication/Graphic Circular") ?? UIImage()
+            t.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+            template = t
+        case .graphicRectangular:
+            break
+        case .graphicExtraLarge:
+            break
+        @unknown default:
+            break
+        }
+
+        return template
     }
 }
