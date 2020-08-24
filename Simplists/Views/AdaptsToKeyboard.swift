a015180c389133d4a871d29f2fd537c6880c645a
Tom Hartnett <tomhartnett@gmail.com>
2020-08-24 07:43:42 -0500

    Updated comments in AdaptsToKeyboard view modifier.

--- Simplists/Views/AdaptsToKeyboard.swift
+++ Simplists/Views/AdaptsToKeyboard.swift
@@ -9,7 +9,8 @@ import Combine
 import SwiftUI
 
 // https://stackoverflow.com/questions/56491881/move-textfield-up-when-the-keyboard-has-appeared-in-swiftui
-// Note, not the accepted answer, but it works. Maybe not on iOS 14; the simulator at least; as of Xcode 12 beta 4.
+// Note, not the accepted answer, but it works. This solution is only necessary on iOS 13, which doesn't avoid the
+// keyboard automatically. See version check below.
 // TODO: revisit this code with newer Xcode 12 betas and/or when deployment target is raised to iOS 14.
 struct AdaptsToKeyboard: ViewModifier {
     @State var currentHeight: CGFloat = 0
@@ -20,11 +21,14 @@ struct AdaptsToKeyboard: ViewModifier {
                 .padding(.bottom, self.currentHeight)
                 .animation(.easeOut(duration: 0.16))
                 .onAppear(perform: {
-                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillShowNotification)
+                    NotificationCenter.Publisher(center: NotificationCenter.default,
+                                                 name: UIResponder.keyboardWillShowNotification)
                         .compactMap { notification in
                             notification.userInfo?["UIKeyboardFrameEndUserInfoKey"] as? CGRect
                     }
                     .map { rect in
+                        // FIXME: this is better but not perfect on iOS 14. As this is essentially a no-op and iOS 14
+                        // should avoid the keyboard for us, I consider remaining jankiness to be beta issues with 14.
                         if #available(iOS 14.0, *) {
                             return CGFloat.zero
                         } else {
@@ -33,7 +37,8 @@ struct AdaptsToKeyboard: ViewModifier {
                     }
                     .subscribe(Subscribers.Assign(object: self, keyPath: \.currentHeight))
 
-                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillHideNotification)
+                    NotificationCenter.Publisher(center: NotificationCenter.default,
+                                                 name: UIResponder.keyboardWillHideNotification)
                         .compactMap { notification in
                             CGFloat.zero
                     }
