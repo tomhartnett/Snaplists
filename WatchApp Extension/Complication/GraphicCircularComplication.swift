8ec7fac0f7097485dd31e092750128952c5d4f83
Tom Hartnett <tomhartnett@gmail.com>
2022-12-10 13:56:05 -0600

    Watch complication updates (#23)

---
+++ WatchApp Extension/Complication/GraphicCircularComplication.swift
@@ -0,0 +1,33 @@
+//
+//  GraphicCircularComplication.swift
+//  WatchApp
+//
+//  Created by Tom Hartnett on 12/9/22.
+//
+
+import ClockKit
+import SwiftUI
+
+struct GraphicCircularComplication: View {
+    @Environment(\.complicationRenderingMode) var mode
+
+    var body: some View {
+        ZStack {
+            if mode == .fullColor {
+                Color("GraphicComplicationBackground")
+            }
+
+            Image("Complication/Graphic Circular")
+        }
+    }
+}
+
+struct GraphicCircularComplication_Previews: PreviewProvider {
+    static var previews: some View {
+        CLKComplicationTemplateGraphicCircularView(GraphicCircularComplication())
+            .previewContext()
+
+        CLKComplicationTemplateGraphicCircularView(GraphicCircularComplication())
+            .previewContext(faceColor: .blue)
+    }
+}
