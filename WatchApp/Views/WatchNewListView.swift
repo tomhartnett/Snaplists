7a3df769cd432021f7884522ae281cd98969ee62
Tom Hartnett <tomhartnett@gmail.com>
2024-02-03 11:38:25 -0600

    Changes to new list view on watch.

--- WatchApp/Views/WatchNewListView.swift
+++ WatchApp/Views/WatchNewListView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 
 struct WatchNewListView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
     @EnvironmentObject var storage: SMPStorage
-    @State private var listTitle = ""
+
+    @State private var newList = SMPList()
 
     let suggestions: [String] = [
         "Grocery",
@@ -22,53 +24,41 @@ struct WatchNewListView: View {
     let listTopID = "ListTopID"
 
     var body: some View {
-        ScrollViewReader { proxy in
-            VStack(alignment: .leading) {
-                List {
-                    Section {
-                        TextField("newlist-name-placeholder".localize(), text: $listTitle)
-                            .id(listTopID)
-                    }
+        VStack(alignment: .leading) {
+            List {
+                Section {
+                    TextField("newlist-name-placeholder".localize(), text: $newList.title)
+                        .id(listTopID)
+                }
 
-                    Section {
-                        Button(action: {
-                            saveNewList()
-                        }, label: {
-                            Text("newlist-save-button-text")
-                                .frame(maxWidth: .infinity)
-                        })
-                        .listRowBackground(
-                            Color("ButtonBlue")
-                                .clipped()
-                                .cornerRadius(8)
-                        )
-                    }
+                Section {
+                    Button(action: {
+                        saveNewList()
+                    }, label: {
+                        Text("newlist-save-button-text")
+                            .frame(maxWidth: .infinity)
+                    })
+                    .listRowBackground(
+                        Color("ButtonBlue")
+                            .clipped()
+                            .cornerRadius(8)
+                    )
+                }
 
-                    Section(header: Text("newlist-suggestions-header-text"), content: {
-                        ForEach(suggestions, id: \.self) { suggestion in
-                            Button(action: {
-                                withAnimation {
-                                    listTitle = suggestion
-                                    proxy.scrollTo(listTopID, anchor: .top)
-                                }
-                            }, label: {
-                                Text(suggestion)
-                            })
-                        }
-                    }).textCase(nil)
+                Section {
+                    WatchListColorsView(list: $newList)
                 }
             }
-            .navigationTitle("New list")
         }
+        .navigationTitle("New list")
     }
 
     private func saveNewList() {
-        guard !listTitle.isEmpty else {
+        guard !newList.title.isEmpty else {
             return
         }
 
-        let list = SMPList(title: listTitle, color: .blue)
-        storage.addList(list)
+        storage.addList(newList)
 
         presentationMode.wrappedValue.dismiss()
     }
